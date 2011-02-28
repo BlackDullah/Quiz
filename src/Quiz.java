aeaabb19088d7aeb24a5a912fac508cb804192f0
Marvin <marvin@imagion.de>
2011-02-28 16:39:45 +0100

    add code to view questions

--- src/Quiz.java
+++ src/Quiz.java
@@ -13,25 +13,37 @@ public class Quiz {
 		// TODO Auto-generated method stub
 
 		BufferedReader reader;
+		String zeile = null;
+		ArrayList<String[]> values = new ArrayList<String[]>();
 
 		try {
 			reader = new BufferedReader(new FileReader("fragen.txt"));
-			String zeile = reader.readLine();
-			ArrayList<String> values = new ArrayList<String>();
-			while (zeile != null) {
-				values.add(zeile.split(";").toString());
+			zeile = reader.readLine();
 
+			while (zeile != null) {
+				values.add(zeile.split(";"));
+				zeile = reader.readLine();
 			}
-			System.out.println(values.size());
-			System.out.println(zeile);
+			// System.out.println(values.size());
 
 		} catch (IOException e) {
-			System.err.println("Error2");
+			System.err.println("Error2 :" + e);
 		}
 
-		System.out.println("Ein kleines Quiz...");
-		frage1();
+		showQuestions(values);
 
+		System.out.println("Ein kleines Quiz...");
+		// frage1();
+	}
+
+	public static void showQuestions(ArrayList<String[]> values) {
+		// System.out.println(values.get(0)[0]);
+		// System.out.println(values.get(1)[0]);
+
+		for (int i = 1; i < 10; i++) {
+			System.out.println(values.get(i)[0]);
+			i++;
+		}
 	}
 
 	public static void frage1() {
@@ -55,11 +67,11 @@ public class Quiz {
 
 	public static void frage2() {
 
-		System.out.println("XXXX");
+		System.out.println("Wie hei§t dieses Progrmm?");
 		Console console = System.console();
 		String myString = console.readLine("Antwort: ");
 
-		if (myString.equalsIgnoreCase("Java")) {
+		if (myString.equalsIgnoreCase("Quiz")) {
 			System.out.println("Richtig");
 
 		}
